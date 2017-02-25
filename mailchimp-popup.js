ac338f15d36b1e9ddfb0a92306b9dd5dacd34c24
0000beesee <0000beesee@gmail.com>
2017-02-25 16:23:07 +0000

    MC-Troublshoot

--- mailchimp-popup.js
+++ mailchimp-popup.js
@@ -1,6 +1,6 @@
 function showMailingPopUp() {
   require(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us14.list-manage.com","uuid":"61aa25325b541e52f240c10d3","lid":"912cacc6a1"}) })
-  document.cookie = "MCEvilPopupClosed=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+  document.cookie = 'MCEvilPopupClosed=;path=/;expires=Thu, 01 Jan 1970 00:00:00 UTC;';
 };
 
 document.getElementById("open-popup").onclick = function() {showMailingPopUp()};
