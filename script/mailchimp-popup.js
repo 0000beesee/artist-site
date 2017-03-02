cf280b7829cb0311db85f6770d63c769e9779cf9
0000beesee <0000beesee@gmail.com>
2017-03-02 22:50:48 +0000

    added closure to mailchimp-popup

--- script/mailchimp-popup.js
+++ script/mailchimp-popup.js
@@ -1,3 +1,5 @@
+(function(){
+
 function showMailingPopUp() {
   require(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us14.list-manage.com","uuid":"61aa25325b541e52f240c10d3","lid":"912cacc6a1"}) })
   document.cookie = 'MCEvilPopupClosed=;path=/;expires=Thu, 01 Jan 1970 00:00:00 UTC;';
@@ -5,3 +7,4 @@ function showMailingPopUp() {
 
 document.getElementById("open-popup").onclick = function() {showMailingPopUp()};
 document.getElementById("open-popup-2").onclick = function() {showMailingPopUp()};
+})();
